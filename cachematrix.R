f91bb8b90766c7276dcc24e698c7ff5d6762e695
Andrew L Lin <landy9920@users.noreply.github.com>
2015-01-23 11:06:23 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -43,21 +43,32 @@ cacheSolve <- function(x, ...) {
     x$setinverse(inv)
     inv
 }
-## Unit test run by 2x2 matrix
+
+## Unit test case run by 2x2 matrix
 ## > x = rbind(c(1, 2), c(-1, 3))
 ## > m = makeCacheMatrix(x)
 ## > m$get()
 
-##      [,1] [,2]
-[1,]    1    2
-[2,]   -1    3
+##         [,1] [,2]
+##   [1,]   1    2
+##   [2,]  -1    3
 ## > cacheSolve(m)
-## Unit test run by 3x3 mtrix
+##         [,1] [,2]
+##   [1,]  0.6 -0.4
+##   [2,]  0.2  0.2
+##   cacheSolve(m)
+##   getting cached data.
+##         [,1] [,2]
+##   [1,]  0.6 -0.4
+##   [2,]  0.2  0.2
+##
+
+## Unit test case run by 3x3 mtrix
 ## > x = rbind(c(1, -1/4, 0), c(-1/4, 0, 1), c(0, 2, 1))
 ## > m = makeCacheMatrix(x)
 ## > m$get()
 
-## [,1]  [,2] [,3]
+##       [,1]  [,2] [,3]
 ## [1,]  1.00 -0.25    0
 ## [2,] -0.25  0.00    1
 ## [3,]  0.00  2.00    1
@@ -68,7 +79,7 @@ cacheSolve <- function(x, ...) {
 ## [3,]  0.2424242  0.9696970 0.03030303
 ## > cacheSolve(m)
 ## getting cached data.
-##       [,1]            [,2]       [,3]
+##       [,1]       [,2]       [,3]
 ## [1,]  0.9696970 -0.1212121 0.12121212
 ## [2,] -0.1212121 -0.4848485 0.48484848
 ## [3,]  0.2424242  0.9696970 0.03030303
